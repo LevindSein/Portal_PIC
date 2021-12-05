25afa62dec36b8b01500fefbb3ec6155ae636e0f
Levind_Sein <levindsein@gmail.com>
2021-12-06 05:41:24 +0700

    php artisan down

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -29,8 +29,6 @@
                 <h1 class="error-title text-info">503</h1>
                 <h3 class="text-uppercase error-subtitle">THIS SITE IS GETTING UP IN FEW MINUTES.</h3>
                 <p class="text-muted mt-4 mb-4">PLEASE TRY AFTER SOME TIME</p>
-                <a href="{{url('/')}}" class="btn btn-info btn-rounded waves-effect waves-light mb-5">Back to home</a> </div>
-
         </div>
     </div>
     <!-- ============================================================== -->
